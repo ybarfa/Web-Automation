1b42fd42b281a5e54b390ee12bb285146852692a
ybarfa <barfayash2@gmail.com>
2023-06-03 21:48:45 +0530

    updated

--- my_project/unit_test_cases/nested_frames.py
+++ my_project/unit_test_cases/nested_frames.py
@@ -6,7 +6,7 @@ driver = webdriver.Chrome()
 driver.get("https://the-internet.herokuapp.com/")
 driver.maximize_window()
 
-element=driver.find_element(By.XPATH,'//a[@href="/nested_frames"]')
+element = driver.find_element(By.XPATH, '//a[@href="/nested_frames"]')
 element.click()
 
 frame_top = driver.find_element(By.XPATH, '//frame[@name="frame-top"]')
@@ -20,7 +20,7 @@ print(text4)
 driver.switch_to.parent_frame()
 frame_middle = driver.find_element(By.XPATH, '//frame[@name="frame-middle"]')
 driver.switch_to.frame(frame_middle)
-text=driver.find_element(By.XPATH,'//div[@id="content"]').text
+text = driver.find_element(By.XPATH, '//div[@id="content"]').text
 print(text)
 
 driver.switch_to.parent_frame()
@@ -32,6 +32,6 @@ print(text2)
 driver.switch_to.default_content()
 frame_bottom = driver.find_element(By.XPATH, '//frame[@name="frame-bottom"]')
 driver.switch_to.frame(frame_bottom)
-text3=driver.find_element(By.XPATH, '//body[contains(text(), "BOTTOM")]').text
+text3 = driver.find_element(By.XPATH, '//body[contains(text(), "BOTTOM")]').text
 print(text3)
 time.sleep(3)
